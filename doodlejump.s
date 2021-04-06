3fb6fcff4c366a84e0188ac60428a251933b56f0
Sasan Makvandi <sasan.makvandi@mail.utoronto.ca>
2021-04-06 00:20:16 -0400

    just a few changes before I change a lot

--- doodlejump.s
+++ doodlejump.s
@@ -42,13 +42,18 @@
 #
 .data
 	displayAddress:	.word	0x10008000
-	
+	four: .word 4
+	thirty: .word 120
 .text
 	lw $t0, displayAddress	# $t0 stores the base address for display
 	li $t1, 0xfffaf5	# $t1 stores the white colour code
 	li $t4, 0x8ec27e	# $t2 stores the green colour code
 	li $t3, 0xff0000	# $t3 stores the red colour code
 	li $t6, 0x0000ff
+	li $t8, 4098
+	
+	
+	# I will be using $t8 for the position of the character
 	
 	sw $t1, 0($t0)	 # paint the first (top-left) unit red. 
 		
@@ -72,29 +77,42 @@
 		j DONE #jump to done
 	DONE:
 	
-	
+	SETTINGTHINGS:
+		
 	#making the doodle
 	lw $t0, displayAddress
-	LOOPINT2:
-		li $t2, 0
-		sw $t1, 0($t0)
+	MAKECHAR:
+		li $t2, 0 # loop variant
+		sw $t1, 0($t0) #store the array address in $t1
 	WHILE3:
 		sw $t1, 0($t0)
 		addi $t2, $t2, 4
 		add $t0, $t0, 4
-		beq $t2, 3760, MAKINGHEAD
+		beq $t2, 3760, MAKINGHEAD #making the head of the character
+		li $s2, 4
+		mult $t8, $s2
+		mfhi $s2
+		addi $s1, $s2, 120 #finding where the position of the body should be
+		beq $t2, $s1 , MAKEBODY #adding 32 *4 to get the exact position on the row bellow
 		beq, $t2, 4092, DONE3
 		
 		j WHILE3
 		
 	MAKINGHEAD:
-		sw $t6, 0($t0)
-		sw $t6, 4($t0)
-		sw $t6, 8($t0)
-		sw $t6, 12($t0)
+		sw $t8, ($t0) #will be the pointer for the character
+		sw $t6, ($t0) #next following will be the painting of the head
 		add $t0, $t0, 4
+		sw $t6, ($t0)
 		addi $t2, $t2, 4
+		j WHILE3 #jump back to the loop to paint the rest
+	
+	MAKEBODY:
+		sw $t6, ($t0)
+		add $t0, $t0, 8
+		sw $t6, ($t0)
+		addi $t2, $t2, 8
 		j WHILE3
+	
 	DONE3:
 	
 	
@@ -104,7 +122,7 @@
 	
 	
 	
-	LOOP2:
+	MAKEPLAT:
 		lw $t0, displayAddress 
 		li $t2, 0 #load the value 0 for the loop
 		addi $a1, $zero, 32 
@@ -143,6 +161,13 @@
 	THEN2:
 		j DONE2
 	DONE2:
+	
+	
+	
+	
+	
+	
+	MAINLOOP:
 		
 		
 	
